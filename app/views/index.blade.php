fbe40fe311cc5cf9fedc93b04b1397f8775bf11d
Conar Welsh <conar@Conars-iMac.local>
2013-09-25 16:35:17 -0600

    initial commit

---
+++ app/views/index.blade.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <title>Theme Template for Bootstrap</title>
+
+    <link href="{{ asset('css/site.css') }}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <img class="logo pull-right brand-border-right" src="{{ asset('img/logo-primary.jpg') }}" />
+      </div>
+
+      <div class="row">
+        <address class="pull-right">
+          3350 Brighton Blvd<br />
+          Denver, CO 80216
+        </address>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+
+      <div class="row">
+
+        <div class="col-lg-6 col-md-6">
+          <div id="source-vendors">
+            <img src="{{ asset('img/Boxcar_7.jpg') }}" />
+            <img src="{{ asset('img/CrookedStave_2.jpg') }}" />
+            <img src="{{ asset('img/Mondo_3.jpg') }}" />
+            <img src="{{ asset('img/Oak_2.jpg') }}" />
+            <img src="{{ asset('img/Mondo_3.jpg') }}" />
+            <img src="{{ asset('img/Boxcar_7.jpg') }}" />
+            <img src="{{ asset('img/CrookedStave_2.jpg') }}" />
+            <img src="{{ asset('img/Oak_2.jpg') }}" />
+            <img src="{{ asset('img/CrookedStave_2.jpg') }}" />
+            <img src="{{ asset('img/Boxcar_7.jpg') }}" />
+            <img src="{{ asset('img/Oak_2.jpg') }}" />
+            <img src="{{ asset('img/Mondo_3.jpg') }}" />
+          </div>
+        </div>
+
+        <div class="col-lg-6 col-md-6">
+          <p class="lead">
+            Welcome to The Source, a highly-anticipated artisan food market that occupies a former
+            1880‘s brick foundry building on Brighton Boulevard in Denver’s River North <abbr title="River North">(RiNo)</abbr> 
+            District. More than two years in planning, The Source has become a collective of food 
+            artisans offering visitors everything from freshly baked bread to craft cocktails, from 
+            street tacos to contemporary flower arrangements.
+          </p>
+          <p>
+            Among The Source’s 15 tenants are Acorn, a wood-fired restaurant and cocktail bar 
+            owned by the team behind Oak at Fourteenth; Comida, a modern Mexican taqueria; 
+            CapRock Farm Bar, a crafted cocktail and juice bar; and Babette’s Artisan Breads, a 
+            traditional French bakery. In the Commissary Space of the building are MeatHead, a 
+            butcher; Americanum Provisions, a specialty produce market; and floral shop Beet & 
+            Yarrow. Tenants also include Super Ordinary Gallery, a design store and exhibit space; 
+            the Proper Pour, an independent wine and spirits bottle shop, and Crooked Stave 
+            Artisan Beer Project, a cultish brewery specializing in funky and sour beers. Also setting 
+            up shop at The Source are Boulder-based Boxcar Coffee Roasters and Mondo Market, 
+            a cheese, spice, and specialty food store.
+          </p>
+          <p>
+            The Source offers a one-stop shopping experience for the food-obsessed while also 
+            providing some of Denver’s best artisans with an outlet for their crafts. The openness 
+            of the building speaks to its community-oriented philosophy. It was repurposed under 
+            the guidance of architect Stephen Dynia with a strong commitment to preserving its 
+            industrial identity while adapting it for its current use. The industrial design with clear 
+            layers of new materials complementing the original shell conveys the integrity and 
+            edginess both of the tenants themselves and of this new marketplace.
+          </p>
+        </div>
+      </div>
+
+      <section id="the-source">
+        <div class="building"></div>
+        <div class="overlay"></div>
+        <div class="caption">
+          <div class="brand-border-left">
+            <p>
+              The Source logo was developed with the idea of space and vertical integration in addition to the utilitarian first use of the
+            </p>
+            <h3>original</h3>
+            <h1>building</h1>
+          </div>
+        </div>
+      </section>
+
+      <hr />
+
+      <footer>
+
+      </footer>
+    </div>
+    
+  </body>
+</html>
