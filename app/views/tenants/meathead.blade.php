678760d895c374d38f2e5258a9eea76df4f96a5e
Conar Welsh <conar@sellwelldesigns.com>
2013-12-16 09:44:37 -0700

    fix typo

--- app/views/tenants/meathead.blade.php
+++ app/views/tenants/meathead.blade.php
@@ -4,7 +4,7 @@
     <h1>Meathead</h1>
     
     <div class="well tenant-content">
-      <p>Meathead is the in-house butcher at The Source with a bit of comedy and traditional flair. Owner Kevin Klinger comes from a long line of butchers and learned his trade the traditional way, by doing it for over three decades. Beef hanging on hooks, not in plastic or in a box, chicken fresh from the farm and a real connection to the trade is what this meat counter is all about. Meathead aims to be a traditional and vert transparent butcher shop.</p>
+      <p>Meathead is the in-house butcher at The Source with a bit of comedy and traditional flair. Owner Kevin Klinger comes from a long line of butchers and learned his trade the traditional way, by doing it for over three decades. Beef hanging on hooks, not in plastic or in a box, chicken fresh from the farm and a real connection to the trade is what this meat counter is all about. Meathead aims to be a traditional and very transparent butcher shop.</p>
       <p>Meathead sources its ethically and humanely raised and processed meats from suppliers like Four Sisters Farm and Tender Belly.</p>
 
       <p>
