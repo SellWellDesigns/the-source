73adb86d95092afaab715948c890ea23dee6e103
Conar Welsh <conar@sellwelldesigns.com>
2014-01-06 09:50:51 -0700

    switch instagram setup to user feed

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'thesource',
+			'database'  => 'the-source',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
