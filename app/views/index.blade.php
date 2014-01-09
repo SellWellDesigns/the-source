4a3726340c8d4342b88155a57edb2ad084471adf
Conar Welsh <conar@sellwelldesigns.com>
2014-01-08 22:38:48 -0700

    update time

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -41,7 +41,7 @@
           <h3>hours</h3>
 
           <p class="lead">
-            Monday - Friday 8:00am - 11:00pm<br />
+            Monday - Friday 9:00am - 11:00pm<br />
             Saturday - Sunday 9:00am - 11:00pm
           </p>
 
