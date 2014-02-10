ba7bc324765ca37ba459bc6820bee705be5b50de
Conar Welsh <conar@sellwelldesigns.com>
2014-02-10 12:19:48 -0700

    update babettes url

--- app/views/tenants/babettes.blade.php
+++ app/views/tenants/babettes.blade.php
@@ -8,7 +8,7 @@
       <p>Babettes Artisan Bakery is a boulangerie focusing on french country breads ranging from the classic Pain Natural to a whole grain French Seeded Rye. Babettes also provides french morning pastries such as croissants, morning buns, and pain aux raisins. Baker Steve Scott is well known for his dark crust created through hydration, fermentation, and proper baking time and temperatures that produces a well hydrated bread throughout with flavor caramelized into the crust. While many of the offerings are consistent from day to day, Scott makes an effort to add in additional french varieties for his customers on a regular basis. We look forward to seeing you here at Babettes for a taste of our very own local boulangerie.</p>
 
       <p>
-        <a class="btn btn-lg" href="http://babettebread.com/" title="http://babettebread.com/">
+        <a class="btn btn-lg" href="http://www.babettesbakery.com/" title="http://www.babettesbakery.com/">
           <i class="icon-globe"></i>
         </a>
         <a class="btn btn-lg" href="https://www.facebook.com/babettesartisanbreads">
